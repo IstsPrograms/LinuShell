785c91804dd2a7bee6b78cf260f3bc281b0ed891
IstsPrograms <97103242+IstsPrograms@users.noreply.github.com>
2022-12-07 18:58:44 +0300

    -

--- LinuShell/ExecuteEngines.cs
+++ LinuShell/ExecuteEngines.cs
@@ -4,18 +4,6 @@ namespace ExecuteEngines
 {
     public class Engines
     {
-        public static void sh()
-        {
-            string scriptToExecute = Environment.GetEnvironmentVariable("runApp");
-            if(Environment.OSVersion.Platform == PlatformID.Unix)
-            {
-                Process.Start($"/etc/bash.bashrc ", $"{Environment.CurrentDirectory}/{scriptToExecute}");
-                Engine.OutputSysNE("[SH] Script executing started!");
-            }
-            else
-            {
-                Engine.OutputSysWE("[SH] Incompatible platform");
-            }
-        }
+
     }
 }
