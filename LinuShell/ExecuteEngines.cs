70f6ce03940621bd5dfa7d77abb5b655d523ada0
IstsPrograms <97103242+IstsPrograms@users.noreply.github.com>
2022-12-07 15:32:04 +0300

    Добавьте файлы проекта.

---
+++ LinuShell/ExecuteEngines.cs
@@ -0,0 +1,21 @@
+﻿using Hydrium;
+using System.Diagnostics;
+namespace ExecuteEngines
+{
+    public class Engines
+    {
+        public static void sh()
+        {
+            string scriptToExecute = Environment.GetEnvironmentVariable("runApp");
+            if(Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                Process.Start($"bash {Environment.CurrentDirectory}/{scriptToExecute}");
+                Engine.OutputSysNE("[SH] Script executing started!");
+            }
+            else
+            {
+                Engine.OutputSysWE("[SH] Incompatible platform");
+            }
+        }
+    }
+}
---
+++ LinuShell/Program.cs
@@ -0,0 +1,23 @@
+﻿using OperManager;
+using Cmds;
+using Hydrium;
+using Services;
+using ExecuteEngines;
+CommandsR.sys();
+string cPath = string.Empty;
+if(Environment.UserName == "root")
+{
+    cPath = "/root/";
+} 
+else
+{
+    cPath = $"/home/{Environment.UserName}/";
+}
+Environment.CurrentDirectory = cPath;
+Engine.OutputSysNE("[PCONF] Path Configured");
+Delegate[] cmds = { CommandsR.cd, CommandsR.dir, CommandsR.cls, CommandsR.ping, CommandsR.shutdown, CommandsR.fget, CommandsR.sys, CommandsR.echo, CommandsR.getd, CommandsR.to, CommandsR.mkzip, CommandsR.unzip, CommandsR.rm, CommandsR.write, CommandsR.mkdir, CommandsR.mkfile, CommandsR.help, CommandsR.ls, CommandsR.clear, CommandsR.about, CommandsR.date};
+Action[] services = { };
+Delegate[] engines = { Engines.sh};
+Operture invokeMgr = new Operture(cmds, services, engines);
+await invokeMgr.MainCmd();
+Console.ReadKey();
